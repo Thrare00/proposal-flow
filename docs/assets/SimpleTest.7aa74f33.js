9d1f47cee782df56b2d700301822099cd13b87f9
thrare00 <rareearthcontracting@gmail.com>
2025-09-25 11:46:58 -0400

    Deploy: refresh docs

--- docs/assets/SimpleTest.7aa74f33.js
+++
@@ -1,2 +0,0 @@
-import{j as e}from"./main.f7b3b261.js";import"./react-vendor.36470f4f.js";function n(){return e.jsx("div",{style:{display:"flex",justifyContent:"center",alignItems:"center",minHeight:"100vh",fontSize:"2rem"},children:"Build Successful!"})}export{n as default};
-//# sourceMappingURL=SimpleTest.7aa74f33.js.map
--- docs/assets/TestPage.fdf7681b.js
+++
@@ -1,2 +0,0 @@
-import{j as e}from"./main.f7b3b261.js";import"./react-vendor.36470f4f.js";const r=()=>e.jsxs("div",{children:[e.jsx("h1",{children:"Test Page"}),e.jsx("p",{children:"This is a test page to verify the build process."})]});export{r as default};
-//# sourceMappingURL=TestPage.fdf7681b.js.map
